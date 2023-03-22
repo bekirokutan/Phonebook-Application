136df41f1bd0845436f866777276dfb703af99c2
Blgsyrc0560 <113426804+Blgsyrc0560@users.noreply.github.com>
2023-03-22 11:44:56 +0300

    Phonebook project files added

---
+++ 202-Terraform-Phonebook-Application-deployed-on-AWS/tf-files/var.tf
@@ -0,0 +1,16 @@
+variable "git-name" {
+  default = "bekirokutan"
+}
+
+variable "git-token" {
+  default = "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"
+}
+
+variable "key-name" {
+  default = "first-key"
+}
+
+variable "hosted-zone" {
+  default = "bekirokutan.com"
+}
+
