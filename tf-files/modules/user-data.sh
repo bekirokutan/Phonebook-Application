ce48de087b402240230ce64e40930f28546b8e18
Bekir OKUTAN <113426804+bekirokutan@users.noreply.github.com>
2023-03-23 18:40:08 +0300

    Update user-data.sh

--- tf-files/modules/user-data.sh
+++ tf-files/modules/user-data.sh
@@ -6,5 +6,5 @@ pip3 install flask_mysql
 yum install git -y
 TOKEN=${user-data-git-token}
 USER=${user-data-git-name}
-cd /home/ec2-user && git clone https://$TOKEN@github.com/$USER/phonebook.git
-python3 /home/ec2-user/phonebook/phonebook-app.py
+cd /home/ec2-user && git clone https://$TOKEN@github.com/$USER/Phonebook-Application.git
+python3 /home/ec2-user/Phonebook-Application/phonebook-app.py
